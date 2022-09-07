4c5c67fa897a479d5bf51d3e41aa8e464e500137
leegwonseok <leestart1023@gmail.com>
2022-09-07 15:07:37 +0900

    chore: DB config 세팅

---
+++ api/config/config.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv");
+const env = process.env;
+const PORT = env.SERVER_PORT || 8080;
+
+dotenv.config();
+const development = {
+  username: env.MYSQL_USERNAME,
+  password: env.MYSQL_PASSWORD,
+  database: env.MYSQL_NAME,
+  host: env.MYSQL_HOST,
+  dialect: "mysql",
+  port: env.MYSQL_PORT,
+  logging: false,
+};
+
+const production = {
+  username: env.MYSQL_USERNAME,
+  password: env.MYSQL_PASSWORD,
+  database: env.MYSQL_NAME,
+  host: env.MYSQL_HOST,
+  dialect: "mysql",
+  port: env.MYSQL_PORT,
+  logging: false,
+};
+
+const test = {
+  username: env.MYSQL_USERNAME,
+  password: env.MYSQL_PASSWORD,
+  database: env.MYSQL_NAME,
+  host: env.MYSQL_HOST,
+  dialect: "mysql",
+  port: env.MYSQL_PORT,
+  logging: false,
+};
+
+module.exports = { development, production, test, PORT };
