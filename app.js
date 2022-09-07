56343008c546a2ef321e2aab914529de844aab64
leegwonseok <leestart1023@gmail.com>
2022-09-07 17:59:42 +0900

    feat: 주석 정리

--- app.js
+++ app.js
@@ -7,33 +7,36 @@ const postRouter = require('./api/routes/posts');
 dotenv.config();
 const app = express();
 
-// DB 연결
 app.set('port', process.env.SERVER_PORT || 8080);
 
+// DB 연결
 sequelize.sync({ force: false })
   .then(() => {
     console.log('◆ DB Connect!');
   })
-  .catch((err) => {
-    console.log(err);
+  .catch(e => {
+    console.log(e);
   });
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//app.use('/api/posts', postRouter);
+// Router
+app.use('/api/posts', postRouter);
 
-// 404 핸들링
+// 404 Handler
 app.use('*', (req, res, next) => {
   res.status(404).send('404 not found');
 });
 
-// error 핸들링
+// error Handler
 app.use((err, req, res, next) => {
   console.error(err);
   res.sendStatus(500);
 })
 
+// Connect Server
 app.listen(app.get('port'), () => {
   console.log(`Serving on port ${app.get('port')}`);
 });
